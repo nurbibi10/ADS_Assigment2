1cfba20833a70b3c8e78cc192397b8d13b5a61c7
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-04-24 08:46:45 +0500

    basic main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.addElement(1);
+        myLinkedList.addElement(2.5);
+        myLinkedList.addElement(3);
+        myLinkedList.addElement(4.7);
+        myLinkedList.addElement('A');
+
+        System.out.println("Index of an index 3:" + myLinkedList.getData(3));
+        myLinkedList.remove(3);
+        System.out.println("Index of an index 3:" + myLinkedList.getData(3));
     }
 }
