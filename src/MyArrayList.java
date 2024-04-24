46213a69f778af1e08164c62aaf5f55d23fdf167
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-04-24 08:44:06 +0500

    addelement method

---
+++ src/MyArrayList.java
@@ -0,0 +1,15 @@
+
+public class MyArrayList<T> implements MyList<T> {
+    private T[] arr;
+    private int size;
+
+    public MyArrayList() {
+        arr = (T[]) new Object[5];
+        size = 0;
+    }
+    @Override
+    public void addElement(T data) {
+        //some pretty code goes here
+    }
+    //some pretty code goes here
+}
