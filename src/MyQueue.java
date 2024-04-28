54a3f4254c971c998e1229d83ae886a908410e9f
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-04-29 00:41:59 +0500

    queue functions

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,2 +1,33 @@
-public class MyQueue {
+public class MyQueue<T>{
+    private MyArrayList<T> arr;
+
+    public MyQueue() {
+        arr = new MyArrayList<>();
+    }
+
+    public void enQueue(T data) {
+        arr.addLast(data);
+    }
+
+    public T deQueue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        T first = arr.getFirst();
+        arr.removeFirst();
+        return first;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        return arr.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return arr.size() == 0;
+    }
 }
