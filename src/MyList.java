f6e9fd403c40d094794ae5193643f32fc70e6ebf
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-04-29 00:23:56 +0500

    all function

--- src/MyList.java
+++ src/MyList.java
@@ -1,5 +1,22 @@
 
-public interface MyList<T>{
+public interface MyList<T> {
     void addElement(T data);
-    //some pretty methods goes here
+    void add(T item);
+    void add(int index, T item);
+    void set(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(Object object);
+    int lastIndexOf(Object object);
+    boolean exists(Object object);
+    Object[] toArray();
+    void clear();
+    int size();
 }
